085a1ced703a7d02708b290b203b666663114f23
sanas0101 <sanashaikhar30@gmail.com>
2023-05-02 21:51:01 +0530

    Add files via upload

---
+++ Anagram.cpp
@@ -0,0 +1,15 @@
+bool isAnagram(string a, string b){
+        
+        
+    sort(a.begin(),a.end());
+    sort(b.begin(),b.end());
+    if(a==b)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+        
+    }
